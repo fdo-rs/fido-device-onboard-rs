e102b551d4b4aa8b9440052cc9b074e1f1d34d11
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-03-20 16:19:49 +0100

    Remove blanc AEAD implementation
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- http-wrapper/src/lib.rs
+++ http-wrapper/src/lib.rs
@@ -21,13 +21,6 @@ impl EncryptionKeys {
     fn encrypt(&self, plaintext: &[u8]) -> Result<Vec<u8>, CryptoError> {
         match self {
             EncryptionKeys::None => Ok(plaintext.to_vec()),
-            EncryptionKeys::AEAD(_) => {
-                println!("Intended to encrypt");
-                // Do a fake encrypt
-                let mut ciphertext = plaintext.to_vec();
-                ciphertext.insert(0, b'E');
-                Ok(ciphertext)
-            }
             _ => todo!(),
         }
     }
@@ -35,15 +28,6 @@ impl EncryptionKeys {
     fn decrypt(&self, ciphertext: &[u8]) -> Result<Vec<u8>, CryptoError> {
         match self {
             EncryptionKeys::None => Ok(ciphertext.to_vec()),
-            EncryptionKeys::AEAD(_) => {
-                println!("Intended to decrypt");
-                // Do a fake decrypt
-                if ciphertext[0] != b'E' {
-                    return Err(CryptoError);
-                }
-                let plaintext = ciphertext[1..].to_vec();
-                Ok(plaintext)
-            }
             _ => todo!(),
         }
     }
