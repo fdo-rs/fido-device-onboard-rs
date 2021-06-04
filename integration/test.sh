08e4115d4b763bf4497108621479b5509217adcc
Peter Robinson <pbrobinson@gmail.com>
2021-06-04 10:09:13 +0100

    Rename rendezvous_server to fdo-rendezvous-server
    Be more consistend in the naming and use of dashes. Also prepend fdo-
    to the Cargo name so the crate is prefixed with fdo.
    
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

--- integration/test.sh
+++ integration/test.sh
@@ -2,7 +2,7 @@
 
 cd integration/testdata
 
-cargo run --bin owner_tool initialize-device --device-cert-ca-chain keys/device_ca_cert.pem --device-cert-ca-private-key keys/device_ca_key.der --manufacturer-cert keys/manufacturer_cert.pem testdevice1 testdevice1.ov testdevice1.dc --rendezvous-info rendezvous_info.yml
+cargo run --bin owner_tool initialize-device --device-cert-ca-chain keys/device_ca_cert.pem --device-cert-ca-private-key keys/device_ca_key.der --manufacturer-cert keys/manufacturer_cert.pem testdevice1 testdevice1.ov testdevice1.dc --rendezvous-info rendezvous-info.yml
 cargo run --bin owner_tool extend-ownership-voucher testdevice1.ov --current-owner-private-key keys/manufacturer_key.der --new-owner-cert keys/reseller_cert.pem
 cargo run --bin owner_tool extend-ownership-voucher testdevice1.ov --current-owner-private-key keys/reseller_key.der --new-owner-cert keys/owner_cert.pem
 cargo run --bin owner_tool report-to-rendezvous --ownership-voucher testdevice1.ov --owner-private-key keys/owner_key.der --owner-addresses-path owner_addresses.yaml  --wait-time 600
