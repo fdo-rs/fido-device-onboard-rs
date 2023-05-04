76d567d48732ab6283107d3d89539812974af9a5
Sarita Mahajan <sarmahaj@redhat.com>
2023-05-04 09:19:17 +0000

    feat(manufacturing-client): use default iface route if not provided
    - in case of mfg_string_type selected as  MACAddress and user has not
    provided which interface to use then get the default route and use that
    interface to get MACAddress.
    - test is added
    - supports both ipv4 network interfaces
    - updated HOWTO.md accordingly.
    
    Signed-off-by: Sarita Mahajan <sarmahaj@redhat.com>

--- integration-tests/tests/di_diun.rs
+++ integration-tests/tests/di_diun.rs
@@ -241,3 +241,52 @@ async fn test_device_credentials_with_tpm() -> Result<()> {
     }
     Ok(())
 }
+
+#[tokio::test]
+async fn test_device_credentials_generated_with_mac_address_no_user_given_iface() -> Result<()> {
+    let mut ctx = TestContext::new().context("Error building test context")?;
+
+    let mfg_server = ctx
+        .start_test_server(
+            Binary::ManufacturingServer,
+            |cfg| {
+                Ok(cfg.prepare_config_file(None, |cfg| {
+                    cfg.insert("rendezvous_port", "1337");
+                    cfg.insert("diun_key_type", "FileSystem");
+                    cfg.insert("device_identification_format", "MACAddress");
+                    Ok(())
+                })?)
+            },
+            |_| Ok(()),
+        )
+        .context("Error creating manufacturing server")?;
+    ctx.wait_until_servers_ready()
+        .await
+        .context("Error waiting for servers to start")?;
+
+    // Generate device credentials with MAC address as the device identification
+    // and where user has not provided which network interface to be used
+
+    let client_result = ctx
+        .run_client(
+            Binary::ManufacturingClient,
+            Some(&mfg_server),
+            |cfg| {
+                cfg.env("DEVICE_CREDENTIAL_FILENAME", "devicecredential.dc")
+                    .env("DI_MFG_STRING_TYPE", "mac_address")
+                    .env("DIUN_PUB_KEY_INSECURE", "true");
+                Ok(())
+            },
+            Duration::from_secs(5),
+        )
+        .context("Error running manufacturing client")?;
+    client_result
+        .expect_success()
+        .context("Manufacturing client failed")?;
+
+    let dc_path = client_result.client_path().join("devicecredential.dc");
+    L.l(format!("Device Credential should be in {:?}", dc_path));
+    assert!(Path::new(&dc_path).exists());
+
+    Ok(())
+}
